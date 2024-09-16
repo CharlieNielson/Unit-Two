0586d5d9a277139fe1febbf4758982b768e14664
CharlieNielson <charlienielson@mail.weber.edu>
2024-09-16 08:34:38 -0600

    Create Financial calculator.cpp

---
+++ Financial calculator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void printStars(){
+    for(int i = 0; i < 40; i++){
+        cout << "*";
+    }
+    cout << endl;
+}
+
+int main (){
+    printStars();
+}
